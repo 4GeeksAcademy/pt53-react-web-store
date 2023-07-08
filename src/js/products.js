8c4ba6cd123efa0c1ee571335795ffae3001f7b1
Shane B <dotfortun@users.noreply.github.com>
2023-07-08 00:21:29 +0000

    one sec, something broke.

---
+++ src/js/products.js
@@ -0,0 +1,182 @@
+export const productData = [
+  {
+    "barcode": 65364,
+    "image_url": "https://placekitten.com/71",
+    "category": "Clothing",
+    "subcategory": "Mens",
+    "name": "Mens Whatchamacallit",
+    "price": "60.95",
+    "sale_price": "54.99"
+  },
+  {
+    "barcode": 30445,
+    "image_url": "https://placekitten.com/196",
+    "category": "Pets",
+    "subcategory": "Dog",
+    "name": "Dog Paraphernalia",
+    "price": "57",
+    "sale_price": "51.95"
+  },
+  {
+    "barcode": 59787,
+    "image_url": "https://placekitten.com/134",
+    "category": "Clothing",
+    "subcategory": "Mens",
+    "name": "Mens Equipment",
+    "price": "50",
+    "sale_price": "40.99"
+  },
+  {
+    "barcode": 14484,
+    "image_url": "https://placekitten.com/144",
+    "category": "Clothing",
+    "subcategory": "Baby",
+    "name": "Baby Implement",
+    "price": "102.99",
+    "sale_price": "81"
+  },
+  {
+    "barcode": 39927,
+    "image_url": "https://placekitten.com/220",
+    "category": "Home",
+    "subcategory": "Cleaning",
+    "name": "Cleaning Thingamajig",
+    "price": "104",
+    "sale_price": "62.99"
+  },
+  {
+    "barcode": 11518,
+    "image_url": "https://placekitten.com/187",
+    "category": "Activity",
+    "subcategory": "Fitness",
+    "name": "Fitness Instrument",
+    "price": "162.95",
+    "sale_price": "145"
+  },
+  {
+    "barcode": 17816,
+    "image_url": "https://placekitten.com/44",
+    "category": "Electronics",
+    "subcategory": "Phone",
+    "name": "Phone Machine",
+    "price": "48.99",
+    "sale_price": "38.95"
+  },
+  {
+    "barcode": 22257,
+    "image_url": "https://placekitten.com/221",
+    "category": "Automotive",
+    "subcategory": "Engine",
+    "name": "Engine Accessory",
+    "price": "94.99",
+    "sale_price": "56"
+  },
+  {
+    "barcode": 47481,
+    "image_url": "https://placekitten.com/44",
+    "category": "Entertainment",
+    "subcategory": "Party",
+    "name": "Party Widget",
+    "price": "12.99",
+    "sale_price": "10.99"
+  },
+  {
+    "barcode": 16769,
+    "image_url": "https://placekitten.com/115",
+    "category": "Electronics",
+    "subcategory": "Camera",
+    "name": "Camera Paraphernalia",
+    "price": "191",
+    "sale_price": "114.95"
+  },
+  {
+    "barcode": 56128,
+    "image_url": "https://placekitten.com/28",
+    "category": "Health",
+    "subcategory": "Medicine",
+    "name": "Medicine Gear",
+    "price": "83.99",
+    "sale_price": "74.99"
+  },
+  {
+    "barcode": 28763,
+    "image_url": "https://placekitten.com/67",
+    "category": "Home",
+    "subcategory": "Gardening",
+    "name": "Gardening Accessory",
+    "price": "54",
+    "sale_price": "43.99"
+  },
+  {
+    "barcode": 27948,
+    "image_url": "https://placekitten.com/169",
+    "category": "Beauty",
+    "subcategory": "Skin Care",
+    "name": "Skin Care Kit",
+    "price": "42.99",
+    "sale_price": "29.95"
+  },
+  {
+    "barcode": 38239,
+    "image_url": "https://placekitten.com/57",
+    "category": "Activity",
+    "subcategory": "Camping",
+    "name": "Camping Implement",
+    "price": "55.99",
+    "sale_price": "44.95"
+  },
+  {
+    "barcode": 47377,
+    "image_url": "https://placekitten.com/53",
+    "category": "Electronics",
+    "subcategory": "Computer",
+    "name": "Computer Equipment",
+    "price": "94.99",
+    "sale_price": "84"
+  },
+  {
+    "barcode": 11451,
+    "image_url": "https://placekitten.com/189",
+    "category": "Home",
+    "subcategory": "Cooking",
+    "name": "Cooking Gear",
+    "price": "47.95",
+    "sale_price": "32.95"
+  },
+  {
+    "barcode": 18071,
+    "image_url": "https://placekitten.com/83",
+    "category": "Electronics",
+    "subcategory": "Phone",
+    "name": "Phone Apparatus",
+    "price": "125.99",
+    "sale_price": "112"
+  },
+  {
+    "barcode": 42222,
+    "image_url": "https://placekitten.com/16",
+    "category": "Clothing",
+    "subcategory": "Baby",
+    "name": "Baby Instrument",
+    "price": "10",
+    "sale_price": "8"
+  },
+  {
+    "barcode": 22538,
+    "image_url": "https://placekitten.com/158",
+    "category": "Home",
+    "subcategory": "Furniture",
+    "name": "Furniture Kit",
+    "price": "10.95",
+    "sale_price": "6.95"
+  },
+  {
+    "barcode": 14384,
+    "image_url": "https://placekitten.com/164",
+    "category": "Activity",
+    "subcategory": "Sports",
+    "name": "Sports Paraphernalia",
+    "price": "116.95",
+    "sale_price": "92.95"
+  }
+]
